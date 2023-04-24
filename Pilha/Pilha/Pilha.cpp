bfc7b454cf0624c8b696f685b901c9dcebda5d53
Nanda P Caetano <fernandamariacaetanopereira@gmail.com>
2023-04-24 08:54:14 -0300

    implementando exibirElemento()

--- Pilha/Pilha/Pilha.cpp
+++ Pilha/Pilha/Pilha.cpp
@@ -1,4 +1,3 @@
-// Pilha.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
 #include <iostream>
 using namespace std;
 
@@ -8,14 +7,15 @@ struct NO {
 	NO* prox;
 };
 
-NO* topo = NULL;
+NO* inicio = NULL;
+NO* fim = NULL;
 
 // headers
 void menu();
 void inicializar();
-void pop();
-void push();
-void exibirElementos();
+void insere();
+void exibirElemento();
+void remove();
 //--------------------------
 
 
@@ -29,14 +29,13 @@ void menu()
 	int op = 0;
 	while (op != 5) {
 		system("cls"); // somente no windows
-		cout << "Menu Pilha";
+		cout << "Menu Fila";
 		cout << endl << endl;
-		cout << "1 - Inicializar Pilha \n";
-		cout << "2 - Inserir elemento (Push) \n";
-		cout << "3 - Remover elementos (Pop) \n";
-		cout << "4 - Exibir Elementos \n";
+		cout << "1 - Inicializar Fila \n";
+		cout << "2 - Inserir elemento \n";
+		cout << "3 - Remover elemento  \n";
+		cout << "4 - Exibir elementos  \n";
 		cout << "5 - Sair \n";
-
 
 		cout << "Opcao: ";
 		cin >> op;
@@ -45,13 +44,13 @@ void menu()
 		{
 		case 1: inicializar();
 			break;
-		case 2:push();
+		case 2:insere();
 			break;
-		case 3: pop();
+		case 3: remove();
 			break;
-		case 4: exibirElementos();
+		case 4: exibirElemento();
 			break;
-		case 5: 
+		case 5:
 			return;
 		default:
 			break;
@@ -66,83 +65,83 @@ void inicializar()
 
 	// se a lista já possuir elementos
 	// libera a memoria ocupada
-	//variável auxiliar, é possível evitar a perda da referência para o início da lista, o que permite reutilizar a variável "topo" para outras operações na lista.
-	//Quando uma lista é liberada da memória, não é possível mais acessá-la, por isso é importante ter cuidado com a manipulação de ponteiros para evitar vazamento de memória.
-	NO* aux = topo;
+	NO* aux = inicio;
 	while (aux != NULL) {
 		NO* paraExcluir = aux;
 		aux = aux->prox;
 		free(paraExcluir);
 	}
 
-	topo = NULL;
-	cout << "Pilha inicializada \n";
+	inicio = NULL;
+	fim = NULL;
+	cout << "Fila inicializada \n";
 
 }
 
 
-void push()
+void insere()
 {
-	// aloca memória dinamicamente para o novo elemento
+	// aloca memoria dinamicamente para o novo elemento
 	NO* novo = (NO*)malloc(sizeof(NO));
 	if (novo == NULL)
 	{
-		cout << "Erro: memoria insuficiente \n";
 		return;
 	}
 
-	cout << "Digite o valor a ser inserido: ";
+	cout << "Digite o elemento: ";
 	cin >> novo->valor;
+	novo->prox = NULL;
 
-	// insere o novo elemento no início da pilha
-	novo->prox = topo;
-	topo = novo;
-	cout << "Elemento inserido com sucesso \n";
+	// Se a fila estiver vazia, o novo elemento será o inicio e o fim
+	if (inicio == NULL) {
+		inicio = novo;
+		fim = novo;
+	}
+	// Se a fila não estiver vazia, adiciona o novo elemento no final da fila
+	else {
+		fim->prox = novo;
+		fim = novo;
+	}
+
+	cout << "Elemento inserido com sucesso! \n";
+
 
 
 
 }
 
-void pop()
+void remove()
 {
 
+	int valorRemovido = inicio->valor;
 
-	// verifica se a pilha está vazia
-	if (topo == NULL)
-	{
-		cout << "A pilha está vazia." << endl;
-		return;
+	// Se a fila tiver somente 1 elemento, atualiza o inicio e o fim para NULL
+	if (inicio == fim) {
+		NO* paraExcluir = inicio;
+		inicio = NULL;
+		fim = NULL;
+		free(paraExcluir);
+	}
+	// Se a fila tiver mais de 1 elemento, atualiza o inicio
+	else {
+		NO* paraExcluir = inicio;
+		inicio = inicio->prox;
+		free(paraExcluir);
 	}
 
-	// cria um ponteiro auxiliar que aponta para o topo da pilha
-	NO* aux = topo;
-
-	// atribui o valor do campo valor do topo da pilha a uma variável elementoRemovido
-	int elementoRemovido = aux->valor;
-
-	// atribui o valor do campo prox do topo da pilha ao ponteiro topo
-	topo = topo->prox;
-
-	// desaloca a memória ocupada pelo elemento removido
-	free(aux);
-
-	cout << "Elemento " << elementoRemovido << " removido da pilha." << endl;
+	cout << "Elemento " << valorRemovido << " removido com sucesso! \n";
 
 }
 
-void exibirElementos() {
-	//se o totpo for nulo a lista esta vazia
-	if (topo == NULL) {
-		cout << "Lista vazia \n";
-		return;
-	}
-	else {
-		cout << "Elementos: \n";
+void exibirElemento() {
 
-		//variavel auxiliar referenciando o topo
-		NO* aux = topo;
-		//enquanto aux (topo) for diferente de nulo, exiba o valor da auxiliar e auxiliar aponta para o próximo
-		//vai dando voltas até não tiver mais elemtos (Nulo)
+	if (fim == NULL) {
+		cout << "Fila vazia" <<endl;
+	}
+	else
+	{
+		NO* aux = inicio;
+
 		while (aux != NULL) {
 			cout << aux->valor << endl;
 			aux = aux->prox;
